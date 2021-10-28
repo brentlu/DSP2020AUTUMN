30628244af60c38e9da04d9a65110b515071faf8
Brent Lu <brent.lu@intel.com>
2021-10-29 00:07:56 +0800

    hw3: fix Makefile for non-docker build
    Not sure why I need to add these library pathes and linker options to
    Makefile when trying to build mydisambig under Ubuntu 20.04 with srilm
    built with kaldi's script.
    
    Anyway, you don't need this patch if working in TA's docker.

--- dsp_hw3/setup.sh
+++ dsp_hw3/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash +v
 
-export SRILM_REP_PATH="/root/srilm-1.5.10"
+export SRILM_REP_PATH="/opt/kaldi/tools/srilm"
 export MACHINE_TYPE="i686-m64"
 
 if [ ! -v SRILM_BIN_PATH ]
@@ -8,6 +8,9 @@ then
         export SRILM_BIN_PATH="$SRILM_REP_PATH/bin/$MACHINE_TYPE"
         export PATH="$SRILM_BIN_PATH:$PATH"
         export PS1="(srilm) $PS1"
+
+        # makefile
+        export SRIPATH=$SRILM_REP_PATH
 fi
 
 deactivate() {
